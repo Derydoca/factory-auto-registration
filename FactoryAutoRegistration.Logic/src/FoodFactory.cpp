f548c5071618562cc5d301bd74735a87082fb9cf
Derrick <derrick.canfield@gmail.com>
2019-03-02 15:25:32 -0800

    FoodFactory is now a singleton

--- FactoryAutoRegistration.Logic/src/FoodFactory.cpp
+++ FactoryAutoRegistration.Logic/src/FoodFactory.cpp
@@ -1,6 +1,10 @@
 #include "pch.h"
 #include "FoodFactory.h"
 
+FoodFactory::FoodFactory()
+{
+}
+
 std::string FoodFactory::getFactoryStatus()
 {
 	return "* FoodFactory is now open, however, it looks extremely empty inside with the exception of one young employee behind the counter with a nametag that reads \"Dan\". *\n";
@@ -9,4 +13,10 @@ std::string FoodFactory::getFactoryStatus()
 std::shared_ptr<FoodObject> FoodFactory::orderFood(std::string foodType)
 {
 	return nullptr;
+}
+
+FoodFactory & FoodFactory::get()
+{
+	static FoodFactory instance;
+	return instance;
 }
--- FactoryAutoRegistration.Logic/src/FoodFactory.h
+++ FactoryAutoRegistration.Logic/src/FoodFactory.h
@@ -3,10 +3,18 @@
 #include <string>
 #include "FoodObject.h"
 
+typedef std::shared_ptr<FoodObject>(*foodInstanceGenerator)();
+
 class DLLEXPORT FoodFactory
 {
 public:
-	std::string getFactoryStatus();
+	static FoodFactory& get();
 
+	std::string getFactoryStatus();
 	std::shared_ptr<FoodObject> orderFood(std::string foodType);
+
+private:
+	FoodFactory();
+	FoodFactory(const FoodFactory&);
+
 };
