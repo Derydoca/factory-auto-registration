4eb288c0a558992d4f43ec96a92160a54287e8b7
Derrick <derrick.canfield@gmail.com>
2019-03-02 14:32:17 -0800

    Added precompiled header for the logic dll

--- FactoryAutoRegistration.Logic/src/FoodFactory.cpp
+++ FactoryAutoRegistration.Logic/src/FoodFactory.cpp
@@ -1,3 +1,4 @@
+#include "pch.h"
 #include "FoodFactory.h"
 
 std::string FoodFactory::getSomeString()
--- FactoryAutoRegistration.Logic/src/FoodFactory.h
+++ FactoryAutoRegistration.Logic/src/FoodFactory.h
@@ -1,14 +1,10 @@
 #pragma once
+#include "pch.h"
 #include <string>
-
-#if defined DLL_EXPORT
-#define DLLEXPORT __declspec(dllexport)
-#else
-#define DLLEXPORT __declspec(dllimport)
-#endif
 
 class DLLEXPORT FoodFactory
 {
 public:
 	std::string getSomeString();
+
 };
