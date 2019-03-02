4cd5e3e51d55ede2f6babefdf88f96243792bf34
Derrick <derrick.canfield@gmail.com>
2019-03-02 14:55:43 -0800

    You can now request an object from the factory, but there are no concrete implementations of objects to create.

--- FactoryAutoRegistration.Logic/src/FoodFactory.cpp
+++ FactoryAutoRegistration.Logic/src/FoodFactory.cpp
@@ -1,7 +1,12 @@
 #include "pch.h"
 #include "FoodFactory.h"
 
-std::string FoodFactory::getSomeString()
+std::string FoodFactory::getFactoryStatus()
 {
-	return "The food factory is currently closed!";
+	return "* FoodFactory is now open, however, it looks extremely empty inside with the exception of one young employee behind the counter with a nametag that reads \"Dan\". *\n";
+}
+
+std::shared_ptr<FoodObject> FoodFactory::orderFood(std::string foodType)
+{
+	return nullptr;
 }
--- FactoryAutoRegistration.Logic/src/FoodFactory.h
+++ FactoryAutoRegistration.Logic/src/FoodFactory.h
@@ -1,10 +1,12 @@
 #pragma once
 #include "pch.h"
 #include <string>
+#include "FoodObject.h"
 
 class DLLEXPORT FoodFactory
 {
 public:
-	std::string getSomeString();
+	std::string getFactoryStatus();
 
+	std::shared_ptr<FoodObject> orderFood(std::string foodType);
 };
